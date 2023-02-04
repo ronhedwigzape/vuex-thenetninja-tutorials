481e09e25c9ec913b7aa945a041212ee42625382
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-02-04 22:08:42 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div id="app">
+<!--    <header>-->
+<!--      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
+
+<!--      <div class="wrapper">-->
+<!--        <HelloWorld msg="You did it!" />-->
+
+<!--        <nav>-->
+<!--          <router-link to="/">Home</router-link>-->
+<!--          <router-link to="/about">About</router-link>-->
+<!--        </nav>-->
+<!--      </div>-->
+<!--    </header>-->
+
+<!--    <router-view />-->
+  </div>
+</template>
+
+<style scoped>
+
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+</style>
