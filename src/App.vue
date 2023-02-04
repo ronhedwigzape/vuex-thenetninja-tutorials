848645fe213cfc50b07c61c76fc6e82b19de9187
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-02-05 00:18:09 +0800

    Add product lists using `props`

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,45 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+
 </script>
 
 <template>
   <div id="app">
-<!--    <header>-->
-<!--      <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />-->
-
-<!--      <div class="wrapper">-->
-<!--        <HelloWorld msg="You did it!" />-->
-
-<!--        <nav>-->
-<!--          <router-link to="/">Home</router-link>-->
-<!--          <router-link to="/about">About</router-link>-->
-<!--        </nav>-->
-<!--      </div>-->
-<!--    </header>-->
-
-<!--    <router-view />-->
+    <product-list-one v-bind:products="products"></product-list-one>
+    <product-list-two v-bind:products="products"></product-list-two>
   </div>
 </template>
+<script>
+import ProductListOne from "./components/ProductListOne.vue";
+import ProductListTwo from "./components/ProductListTwo.vue";
 
+export default {
+  components: {
+    'product-list-one': ProductListOne,
+    'product-list-two': ProductListTwo
+  },
+  data() {
+    return {
+      products: [
+        {name: 'Banana Skin', price: 20},
+        {name: 'Shiny Star', price: 40},
+        {name: 'Green Shells', price: 60},
+        {name: 'Red Shells', price: 80},
+      ]
+    }
+  }
+}
+</script>
 <style scoped>
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
+body {
+  font-family: Ubuntu;
+  color: #555;
 }
+/*nav a.router-link-exact-active {*/
+/*  color: var(--color-text);*/
+/*}*/
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
+/*nav a.router-link-exact-active:hover {*/
+/*  background-color: transparent;*/
+/*}*/
 
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import './assets/main.css'
-
 new Vue({
   router,
   render: (h) => h(App)
