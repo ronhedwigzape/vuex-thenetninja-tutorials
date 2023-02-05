9e5bc82929f79c1955a106973154e1f065e2803a
Ron Hedwig Zape <ronhedwigaquinozape@gmail.com>
2023-02-05 21:19:34 +0800

    Add vuex and migrate to Vue 3

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -1,13 +1,13 @@
 <template>
-<div id="product-list-one">
-  <h2>Product List One</h2>
-  <ul>
-    <li v-for="product in products">
-      <span class="name">{{ product.name }}</span>
-      <span class="price">$ {{ product.price }}</span>
-    </li>
-  </ul>
-</div>
+  <div id="product-list-one">
+    <h2>Product List One</h2>
+    <ul>
+      <li v-for="product in products">
+        <span class="name">{{ product.name }}</span>
+        <span class="price">$ {{ product.price }}</span>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,7 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 
-Vue.use(VueRouter)
-
-const router = new VueRouter({
-  mode: 'history',
-  base: import.meta.env.BASE_URL,
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
@@ -17,6 +13,7 @@ const router = new VueRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
+
     }
   ]
 })
